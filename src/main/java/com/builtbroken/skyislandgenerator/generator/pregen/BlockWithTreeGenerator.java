bf4d07bc2567b97151e7bc0571626eb2a252b14f
Robert Seifert <rseifert.phone@gmail.com>
2015-08-13 12:45:06 -0400

    Fixed tree island generator, Seems spawn island is not generating and Stone brick island is broken

--- src/main/java/com/builtbroken/skyislandgenerator/generator/pregen/BlockWithTreeGenerator.java
+++ src/main/java/com/builtbroken/skyislandgenerator/generator/pregen/BlockWithTreeGenerator.java
@@ -1,7 +1,6 @@
 package com.builtbroken.skyislandgenerator.generator.pregen;
 
 import com.builtbroken.skyislandgenerator.generator.IslandGenerator;
-import net.minecraft.block.BlockSapling;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 
@@ -14,7 +13,25 @@ public class BlockWithTreeGenerator extends IslandGenerator
     public void generate(World world, int x, int y, int z)
     {
         world.setBlock(x, y, z, Blocks.grass);
-        world.setBlock(x, y + 1, z, Blocks.sapling);
-        ((BlockSapling) Blocks.sapling).func_149879_c(world, x, y + 1, z, world.rand);
+        world.setBlock(x, y + 1, z, Blocks.log);
+        world.setBlock(x, y + 2, z, Blocks.log);
+        for (int j = y + 3; j <= y + 4; j++)
+        {
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int k = z - 1; k <= z + 1; k++)
+                {
+                    if (i == x && k == z)
+                        world.setBlock(i, j, k, Blocks.log);
+                    else
+                        world.setBlock(i, j, k, Blocks.leaves);
+                }
+            }
+        }
+        world.setBlock(x, y + 5, z, Blocks.leaves);
+        world.setBlock(x + 1, y + 5, z, Blocks.leaves);
+        world.setBlock(x - 1, y + 5, z, Blocks.leaves);
+        world.setBlock(x, y + 5, z + 1, Blocks.leaves);
+        world.setBlock(x, y + 5, z - 1, Blocks.leaves);
     }
 }
