b7de713e3ec74bd2c6f5d5b2e1a5fb38de51b7ad
Robert Seifert <rseifert.phone@gmail.com>
2015-08-13 03:19:31 -0400

    [Untested] Added code to generate random locations to create new islands

--- src/main/java/com/builtbroken/skyislandgenerator/handler/IslandManager.java
+++ src/main/java/com/builtbroken/skyislandgenerator/handler/IslandManager.java
@@ -4,10 +4,16 @@ import com.builtbroken.skyislandgenerator.SkyIslandGenerator;
 import com.builtbroken.skyislandgenerator.generator.IslandGenerator;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.ForgeDirection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,9 +27,18 @@ public class IslandManager
     public List<IslandLocation> nextLocations = new ArrayList();
 
     public static final IslandManager INSTANCE = new IslandManager();
+    private static final List<ForgeDirection> dirs = new ArrayList();
 
-    private int gridIterationX = 0;
-    private int gridIterationZ = 0;
+    static
+    {
+        dirs.add(ForgeDirection.EAST);
+        dirs.add(ForgeDirection.WEST);
+        dirs.add(ForgeDirection.NORTH);
+        dirs.add(ForgeDirection.SOUTH);
+    }
+
+    private int ring = 0;
+    private int ring_index = 0;
 
     private IslandManager()
     {
@@ -48,9 +63,30 @@ public class IslandManager
     {
         if (event.phase == TickEvent.Phase.END && event.world.provider.dimensionId == 0 && nextLocations.size() < 5)
         {
+            if (islandMap.isEmpty())
+            {
+                islandMap.put(new IslandLocation(event.world.getSpawnPoint().posX >> 4, event.world.getSpawnPoint().posZ >> 4), null);
+            }
+            for (IslandLocation location : islandMap.keySet())
+            {
+                int originX = (location.x << 4) + 8;
+                int originZ = (location.z << 4) + 8;
 
+                Collections.shuffle(dirs);
+                for (ForgeDirection dir : dirs)
+                {
+                    int nextX = (originX + dir.offsetX * SkyIslandGenerator.DISTANCE_BETWEEN_ISLANDS) >> 4;
+                    int nextZ = (originZ + dir.offsetZ * SkyIslandGenerator.DISTANCE_BETWEEN_ISLANDS) >> 4;
+                    IslandLocation newLocation = new IslandLocation(nextX, nextZ);
+                    if (!isLocationAlreadyInUse(newLocation))
+                    {
+                        nextLocations.add(newLocation);
+                    }
+                }
+            }
         }
     }
+
 
     public IslandLocation getNewIslandLocation()
     {
@@ -66,7 +102,7 @@ public class IslandManager
         if (islandMap.containsKey(location)) return true;
         for (IslandLocation loc : islandMap.keySet())
         {
-            if (Math.abs(loc.x - location.x) <= SkyIslandGenerator.DISTANCE_BETWEEN_ISLANDS || Math.abs(loc.z - location.z) <= SkyIslandGenerator.DISTANCE_BETWEEN_ISLANDS)
+            if ((Math.abs(loc.x - location.x) << 4) <= SkyIslandGenerator.DISTANCE_BETWEEN_ISLANDS || (Math.abs(loc.z - location.z) << 4) <= SkyIslandGenerator.DISTANCE_BETWEEN_ISLANDS)
             {
                 return true;
             }
@@ -74,7 +110,25 @@ public class IslandManager
         return false;
     }
 
-    public void newIsland(World worldObj, int x, int z, String chunkFlat, boolean random)
+    public void newIsland(EntityPlayer player, int chunkX, int chunkZ, String chunkFlat, boolean random)
+    {
+        IslandLocation location = newIsland(player.worldObj, chunkX, chunkZ, chunkFlat, random);
+        if (location != null)
+        {
+            int x = (chunkX << 4) + 8;
+            int z = (chunkZ << 4) + 8;
+            int y = 0;
+            islandMap.put(location, new IslandData(player));
+            MovingObjectPosition mop = player.worldObj.rayTraceBlocks(Vec3.createVectorHelper(x + 0.5, 255, z + 0.5), Vec3.createVectorHelper(x + 0.5, 0, z + 0.5));
+            if (mop != null && mop.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
+            {
+                player.setSpawnChunk(new ChunkCoordinates(x, mop.blockY + 1, z), true);
+                player.setPositionAndUpdate(x, y, z);
+            }
+        }
+    }
+
+    public IslandLocation newIsland(World worldObj, int x, int z, String chunkFlat, boolean random)
     {
         IslandLocation location = new IslandLocation(x, z);
         if (islandTypeMap.containsKey(chunkFlat))
@@ -84,6 +138,10 @@ public class IslandManager
                 gen.generateNoRandom(worldObj, x, z);
             else
                 gen.generate(worldObj, x, z);
+
+            islandMap.put(location, null);
+            return location;
         }
+        return null;
     }
 }
